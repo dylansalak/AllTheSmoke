b3a5acb3496f1467f5f4b39582bd8b79fe4c3501
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-03-09 22:11:34 -0800

    Added other script for Demo Part 2

---
+++ All The Smoke/Assets/Scripts/RotationSamplerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationSamplerFactory : MonoBehaviour
+{
+    public GameObject samplerFab;
+    public AudioPeer peer;
+
+    private int numObj;
+    private GameObject[] samplers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        samplers = new GameObject[peer.sampleScale];
+
+        for(int i = 0; i < peer.sampleScale; ++i)
+        {
+            samplers[i] = Instantiate(
+                samplerFab,
+                new Vector3(0f, 0f, 5f * i),
+                Quaternion.identity,
+                this.gameObject.transform
+            );
+            samplers[i].GetComponent<RotationSampler>().peer = peer;
+            samplers[i].GetComponent<RotationSampler>().sampleNumber = i;
+        }
+    }
+}
