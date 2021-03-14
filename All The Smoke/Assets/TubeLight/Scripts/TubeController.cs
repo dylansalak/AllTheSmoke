7cc80fe96d2b1d3c69506fd5436725910c71f90e
dylansalak <dsalak55@gmail.com>
2021-03-14 16:29:14 -0700

    Visualizer on stage

---
+++ All The Smoke/Assets/TubeLight/Scripts/TubeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeController : MonoBehaviour
+{
+
+    public Vector3 anglesToRotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.Rotate(new Vector3 (1f, 0f, 0f), anglesToRotate.x * Time.deltaTime, Space.World);
+        // this.transform.Rotate(new Vector3 (0f, 1f, 0f), anglesToRotate.y * Time.deltaTime, Space.World);
+        // this.transform.Rotate(new Vector3 (0f, 0f, 1f), anglesToRotate.z * Time.deltaTime, Space.World);
+        this.transform.RotateAround(new Vector3 (0f, 0f, 0f), new Vector3 (0f, 0f, 1f), 90f * Time.deltaTime);
+    }
+}
